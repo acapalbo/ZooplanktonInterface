1ff43c63521c7ace1cf3f9c4bd663a6e3b83d9e4
acapalbo <acapalbo@fau.edu>
2024-09-19 13:55:49 -0400

    first release

--- prepareDataset.m
+++ prepareDataset.m
@@ -9,14 +9,14 @@ mkdir PreparedDataset
 globalIter = 1;
 for k=3:length(Files)
     FolderName=Files(k).name;
-    mkdir(strcat("PreparedDataset/",num2str(k-2)))
+    mkdir(strcat("PreparedDataset/",classNames(k-2)))
     cd (strcat(srcFolder,"/",FolderName))
     imgs = dir('*.*');
     for z=3:length(imgs)
         img = imread(imgs(z).name);
         img = imresize(img,uniformSize,"bilinear");
         % delete(imgs(z).name)
-        imwrite(img,strcat(startingFolder,"/PreparedDataset/",num2str(k-2),"/", ...
+        imwrite(img,strcat(startingFolder,"/PreparedDataset/",classNames(k-2),"/", ...
             num2str(globalIter,'%04.f'),".png"));
         % movefile(imgs(z).name,strcat(num2str(globalIter,'%03.f'),".png"));
         globalIter = globalIter + 1;
