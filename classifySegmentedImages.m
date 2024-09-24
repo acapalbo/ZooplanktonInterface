6bb15128decd0f2115fcf7e987585cb12f8791d1
acapalbo <acapalbo@fau.edu>
2024-09-24 12:58:56 -0400

    version 0.7.1 source code

--- classifySegmentedImages.m
+++ classifySegmentedImages.m
@@ -1,10 +1,11 @@
 function scores = classifySegmentedImages(trainedNet,imds,confidenceThreshold,appAxis,dirLocation)
-    X = readall(imds);
+    % X = readall(imds);
     [l,w] = size(imds.preview);
-    Xtrain = reshape(cat(3,X{:}),[l,w,1,length(X)]);
+    % Xtrain = reshape(cat(3,X{:}),[l,w,1,length(X)]);
     % scores = minibatchpredict(trainedNet,imds);
-    scores = trainedNet.predict(dlarray(double(Xtrain),'SSCB'))
-    [a,b] = max(scores,[],1)
+    % scores = trainedNet.predict(dlarray(double(Xtrain)/255,'SSCB'));
+    scores = minibatchpredict(trainedNet,imds)
+    [a,b] = max(scores,[],2)
     % accuracy = mean(categorical(b)' == imds.Labels)
     % size(scores)
     % classList = (repmat([1:size(scores,1)],length(X),1))';
@@ -12,10 +13,10 @@ function scores = classifySegmentedImages(trainedNet,imds,confidenceThreshold,ap
     % layer = trainedNet.Layers(length(trainedNet.Layers) - 1);
     % classNames = categories(imds.Labels);
     if confidenceThreshold == 0
-        classCounts = zeros(1,size(scores,1));
+        classCounts = zeros(1,size(scores,2));
     else
         % include trash category
-        classCounts = zeros(1,size(scores,1) + 1);
+        classCounts = zeros(1,size(scores,2) + 1);
     end
     % make className directories
     mkdir ClassificationOutput
@@ -23,15 +24,16 @@ function scores = classifySegmentedImages(trainedNet,imds,confidenceThreshold,ap
         mkdir(strcat(dirLocation,'/ClassificationOutput/',num2str(z)))
     end
     % scores = reshape(scores,size(scores,2),[])
-    b = categorical(b);
-    scores = b;
+    bCat = categorical(b);
+    scores = bCat;
     for i = 1:length(scores)
+        a(i)
         if a(i) > confidenceThreshold
             % save image to directory
             classCounts(b(i)) = classCounts(b(i)) + 1;
             copyfile(cell2mat(imds.Files(i)),strcat(dirLocation,'/ClassificationOutput/',string(b(i))))
         else
-            class = size(scores,2) + 1;
+            class = size(classCounts,2);
             classCounts(class) = classCounts(class) + 1;
             copyfile(cell2mat(imds.Files(i)),strcat(dirLocation,'/ClassificationOutput/',num2str(class)))
             % move to low confidence folder
@@ -41,6 +43,7 @@ function scores = classifySegmentedImages(trainedNet,imds,confidenceThreshold,ap
     % give dataset stats and mean confidence for each class
     % X = categorical(1:size(classCounts,2));
     % X = reordercats(X,1:size(classCounts,2));
+    % classCounts
     b = bar(appAxis,1:size(classCounts,2),classCounts);
     xtips2 = b.XEndPoints;
     ytips2 = b.YEndPoints;
