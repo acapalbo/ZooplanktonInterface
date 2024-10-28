8812a7d43e57de4f401df9e7a223d295c78bceb7
acapalbo <acapalbo@fau.edu>
2024-10-28 18:30:22 -0400

    cleanup and added new network

--- videoProcessing/write_avi.m
+++ videoProcessing/write_avi.m
@@ -1,7 +1,10 @@
 % takes filename and grayscaled video (length, width, frame), creates file
 function write_avi(frames,filename)
+    tStart = tic;
     v = VideoWriter(filename,"Grayscale AVI");
     open(v)
     writeVideo(v, frames)
     close(v)
+    tEnd = toc(tic);
+    fprintf("<strong>Write Time: %.3f seconds</strong>",tEnd);
 end
