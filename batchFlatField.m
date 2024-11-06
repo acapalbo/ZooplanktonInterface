b6fac2d05f2dc0c1e014f4f0005a5611868dba33
acapalbo <acapalbo@fau.edu>
2024-11-06 09:48:47 -0500

    minor edits

--- batchFlatField.m
+++ batchFlatField.m
@@ -9,11 +9,6 @@ function batchFlatField()
     for z = 3:length(videoFiles)
         try
             tempVid = read_avi(fullfile(filePath,videoFiles(z).name));
-            calFrame = calibrateV2(tempVid);
-            ffVid = preprocess_video(tempVid,calFrame,1,"precise");
-            fileName = videoFiles(z).name;
-            fileName = strcat(fileName(1:end-4),"precise_ff.avi");
-            write_avi(ffVid,fullfile(filePath,"FFvideos\",fileName))
         catch
             fprintf("<strong>%s</strong> failed to read.\n",videoFiles(z).name);
             continue
@@ -21,7 +16,6 @@ function batchFlatField()
         try
             calFrame = calibrateV2(tempVid);
             ffVid = preprocess_video(tempVid,calFrame,1,"precise");
-
         catch
             fprintf("<strong>%s</strong> failed to process.\n",videoFiles(z).name);
             continue
